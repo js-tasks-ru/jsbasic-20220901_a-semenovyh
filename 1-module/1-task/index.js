c62834acef761806f9f3eadf80a6a5088fc85cc1
Alex Semenovyh <a-semenovyh@ya.ru>
2022-09-08 20:11:20 +0300

    Factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
+let n;
 function factorial(n) {
-  // ваш код...
-}
+	
+		if(n === 0 || n === 1) {
+	
+		return 1;
+		} else {
+			for (i = n-1; i >= 1; i--) {
+				n = n * i;
+				}
+			return n;
+		}
+			
+	}
+	alert(factorial(0));
