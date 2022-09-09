6a8ff102eca71fc025d54d7e0e88a71ec4a51045
Alex Semenovyh <a-semenovyh@ya.ru>
2022-09-09 17:12:37 +0300

    Убрал n

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,15 +1,11 @@
-let n;
 function factorial(n) {
-	
-		if(n === 0 || n === 1) {
-	
+	if (n === 0 || n === 1) {
 		return 1;
-		} else {
-			for (i = n-1; i >= 1; i--) {
-				n = n * i;
-				}
-			return n;
+	} else {
+		for (i = n-1; i >= 1; i--) {
+			n *= i;
 		}
-			
+		return n;
 	}
-	alert(factorial(0));
+}
+alert(factorial(5));
