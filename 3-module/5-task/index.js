0d00cb6ce4e800994ef9617d436131b194a29290
Alex Semenovyh <a-semenovyh@ya.ru>
2022-09-16 22:35:34 +0300

    MinMax

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+	let arr = str
+	.split(' ')
+	.filter(item => (Number(item) && isFinite(item)))
+	.sort( (a, b) => a - b );
+	
+	const min = Number(arr[0]);
+	const max = Number(arr[arr.length - 1]);
+	let result = {min, max};
+	
+return result;
 }
