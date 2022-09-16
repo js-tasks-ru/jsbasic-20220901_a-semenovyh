2af8eee750003b38a662915917d429ea12d63511
Alex Semenovyh <a-semenovyh@ya.ru>
2022-09-16 23:24:35 +0300

    добавил map и убрал приведение к числу в объекте

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -2,10 +2,11 @@ function getMinMax(str) {
 	let arr = str
 	.split(' ')
 	.filter(item => (Number(item) && isFinite(item)))
+	.map(item => +item)
 	.sort( (a, b) => a - b );
 	
-	const min = Number(arr[0]);
-	const max = Number(arr[arr.length - 1]);
+	const min = arr[0];
+	const max = arr[arr.length - 1];
 	let result = {min, max};
 	
 return result;
