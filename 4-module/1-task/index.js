77958a13156ccc6b091c92a41308f95ed9bbd0ea
Alex Semenovyh <a-semenovyh@ya.ru>
2022-09-21 23:15:10 +0300

    makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let newUl = document.createElement('ul');
+  	for (let item of friends) {
+		let newLi = document.createElement('li');
+		newLi.innerHTML = `${item.firstName} ${item.lastName}`;
+		newUl.appendChild(newLi);
+	}
+	return(newUl);
 }
