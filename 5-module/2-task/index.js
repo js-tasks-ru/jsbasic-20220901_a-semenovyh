8c7af0d3e98d49fa4d725a6bb28b23cf054dcc8a
Alex Semenovyh <a-semenovyh@ya.ru>
2022-09-25 22:54:32 +0300

    textHidden

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,18 @@
 function toggleText() {
-  // ваш код...
+	const button = document.querySelector('.toggle-text-button');
+	const text = document.getElementsByTagName('div')[0];
+
+		function handler() {
+			if (text.hidden === true) {
+				text.hidden = false;
+			} else {
+				text.hidden = true;
+			}
+		}
+		/*function handler2() {
+			text.hidden = false;
+		}	*/
+	
+		button.addEventListener("click", handler);
+		//button.addEventListener("click", handler2);
 }
